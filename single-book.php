b6b3cb2f5849448bcd17d1125e8ca1711f21f8e0
ReznichenkoKirill <Kirill.reznicenkoo@gmail.com>
2021-04-16 14:20:03 +0300

    was added archive-book & single-book

---
+++ single-book.php
@@ -0,0 +1,30 @@
+<?php
+get_header();
+
+if (have_posts()) {
+    while (have_posts()) : the_post(); ?>
+        <div class="col-12 d-flex flex-column justify-content-center">
+            <?php
+            the_title('<h2 class="text-center mb-3">', '</h2>');
+            the_post_thumbnail();
+            the_excerpt(); //the_content();
+            ?>
+            <ul class="m-0 p-0 mb-4">
+                <li>Author: <?= get_the_author_link(); ?></li> <!-- get_the_author_link() -->
+                <li>Time: <?php the_time(); ?></li>
+                <li>
+                    <?php
+                    $user = wp_get_current_user();
+                    if (get_the_author() == $user->nickname || current_user_can('edit_published_posts')) {
+                        edit_post_link();
+                    }
+                    ?>
+                </li>
+                <li>Categories: <?php the_category('/'); ?></li>
+                <p class="text-center m-0 p-0"><a href='<?= home_url() ?>' title='To homepage'>Back to homepage</a></p>
+            </ul>
+        </div>
+    <?php endwhile;
+}
+get_footer();
+
