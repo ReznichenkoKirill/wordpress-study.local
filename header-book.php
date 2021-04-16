f5232b6665aa75e9983ef578f23a5c9ce452d664
ReznichenkoKirill <Kirill.reznicenkoo@gmail.com>
2021-04-16 17:42:22 +0300

    added header-book

---
+++ header-book.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+<body class="d-grid book-bg">
+<header class="">
+    <nav class="d-flex justify-content-between align-items-center">
+        <div class="col-4"><a href="<?= home_url(); ?>"><img
+                    src="<?= get_template_directory_uri() ?>/assets/img/book-white.png" alt="book_logo"
+                    class="logo-img"></a></div>
+        <ul class="col-8 d-flex justify-content-center">
+            <li><?php wp_nav_menu(['menu' => 'Top Menu', 'container_class' => 'link-light']); ?></li>
+        </ul>
+    </nav>
+</header>
+<main class="container d-flex flex-column justify-content-center">
+    <?php wp_body_open(); ?>
