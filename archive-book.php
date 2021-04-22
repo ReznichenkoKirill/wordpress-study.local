c0ce1bd7c36f3253411baa4a603a68bae5708eff
ReznichenkoKirill <Kirill.reznicenkoo@gmail.com>
2021-04-22 18:59:23 +0300

    changed templates

--- archive-book.php
+++ archive-book.php
@@ -5,21 +5,29 @@ while (have_posts()) : the_post(); ?>
 
     <div class="col-12 d-flex flex-column justify-content-center mt-4 book-blur">
         <a href="<?php the_permalink(); ?>"><h2 class="text-center mb-3"><?php the_title(); ?></h2></a>
-
         <div class="d-flex justify-content-center mt-3 mb-5">
-            <?php the_post_thumbnail('medium', [ 'class' => 'text-center']); ?>
+            <?php the_post_thumbnail('medium', ['class' => 'text-center']); ?>
         </div>
 
         <?php
-
         the_excerpt();
         ?>
 
         <ul class="m-0 p-0 mb-4">
-            <li>Author: <?php the_field('author'); ?></li>
-            <li>Date: <?php the_field('date_of_book_write'); ?></li>
+            <ul class="m-0 p-0 mb-4">
+                <?php if (!empty(get_field('author'))) : ?>
+                    <li><h6>Author: <?php echo get_field('author') ?></h6></li>
+                <?php else: ?>
+                    <li><h6>Author: <?php echo NO_DATA_MESS ?></h6></li>
+                <?php endif; ?>
+                <?php if (!empty(get_field('date_of_book_write'))) : ?>
+                    <li><h6>Date: <?php echo get_field('date_of_book_write') ?></h6></li>
+                <?php else: ?>
+                    <li><h6>Date: <?php echo NO_DATA_MESS ?></h6></li>
+                <?php endif; ?>
+            </ul>
             <li>
-                <?php echo the_taxonomies(get_the_ID()); ?>
+                <?php the_taxonomies() ?>
             </li>
 
             <?php
@@ -27,13 +35,14 @@ while (have_posts()) : the_post(); ?>
 
             if (get_the_author() == $user->nickname || current_user_can('edit_published_posts')) :?>
                 <li> <?php edit_post_link(); ?> </li>
-            <?php endif;
-
-            if (!is_page()) {
-                the_shortlink('Read more!', 'link', '<p class="text-center m-0 p-0">', '</p>');
-            }
-            ?>
+            <?php endif; ?>
         </ul>
+        <?php if (!is_page()) : ?>
+            <button class="col-2 button text-center m-0 p-0 mx-auto border border-dark">
+                <a class="d-block col-12 bg-dark text-light" href="<?php echo get_page_link(get_the_ID()) ?>">Read
+                    more!</a>
+            </button>
+        <?php endif; ?>
     </div>
 
 <?php
--- single-book.php
+++ single-book.php
@@ -4,106 +4,61 @@ get_header();
 if (have_posts()) {
     while (have_posts()) : the_post(); ?>
         <div class="col-12 d-flex flex-column justify-content-center mt-5 mb-5 book-blur p-5">
-
             <?php
             the_title('<h2 class="text-center mb-3">', '</h2>');
             ?>
-
             <div class="d-flex justify-content-center">
-                <?php the_post_thumbnail('medium', [ 'class' => 'mt-3 mb-5']); ?>
+                <?php the_post_thumbnail('medium', ['class' => 'mt-3 mb-5']);
+                ?>
             </div>
-
             <?php
             the_content();
             ?>
-
-
-            <?php
-            $images = get_field('slider_book');
-            if (!empty($images)) :
-                ?>
-
-                <div id="carouselExampleIndicators" class="carousel slide w-50 mx-auto" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                class="active"
-                                aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <?php
-                        $img_first_el = $images[0];
-                        $img_first_id = $img_first_el['ID'];
-
-                        foreach ($images as $img) :
-                            if ($img['ID'] === $img_first_id) :
-                        ?>
-                                <div class="carousel-item active">
-                            <?php else : ?>
-                                <div class="carousel-item">
-                            <?php endif; ?>
-                            <?php echo wp_get_attachment_image($img['ID'], 'medium', '', ['class' => 'd-block mx-auto']); ?>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon bg-dark h-100" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="next">
-                        <span class="carousel-control-next-icon bg-dark h-100" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            <?php endif; ?>
-
-
-            <ul class="m-0 p-0 mb-4">
-                <li>Author: <?php the_field('author'); ?></li>
-                <li>Date: <?php the_field('date_of_book_write'); ?></li>
-                <li>
-                    <?php echo the_taxonomies(get_the_ID()); ?>
-                </li>
-
-                <?php
-                $user = wp_get_current_user();
-                if (get_the_author() == $user->nickname || current_user_can('edit_published_posts')) :?>
-                    <li> <?php edit_post_link(); ?> </li>
-                <?php endif ?>
             </ul>
-
             <div>
-                <h2>Same books:</h2>
-                <ul>
+                <ul class="m-0 p-0 mb-4">
+                    <?php if (!empty(get_field('author'))) : ?>
+                        <li><h6>Author: <?php echo get_field('author') ?></h6></li>
+                    <?php else: ?>
+                        <li><h6>Author: <?php echo NO_DATA_MESS ?></h6></li>
+                    <?php endif; ?>
+                    <?php if (!empty(get_field('date_of_book_write'))) : ?>
+                        <li><h6>Date: <?php echo get_field('date_of_book_write') ?></h6></li>
+                    <?php else: ?>
+                        <li><h6>Date: <?php echo NO_DATA_MESS ?></h6></li>
+                    <?php endif; ?>
+                </ul>
+                <h5 class="mb-0 pb-0">Same books:</h5>
+                <ul class="m-0 mb-3">
                     <?php
-                    $genres = get_the_terms(get_the_ID(), 'genres');
-                    $arr_genres = [];
+                    $get_book = get_post(get_the_ID());
+                    $genres = get_the_terms($get_book->ID, 'genres');
+                    $arr_genres_id = [];
                     foreach ($genres as $genre) {
-                        $arr_genres[] = $genre->slug;
+                        $arr_genres_slug[] = $genre->slug;
                     }
                     $same_books = get_posts([
-                        'genres' => $arr_genres,
-                        'post_type' => 'book',
-                        'exclude' => get_the_ID(),
+                        'genres' => $arr_genres_slug,
+                        'post_type' => $get_book->post_type,
+                        'exclude' => $get_book->ID,
                         'numberposts' => 5,
                     ]);
-
                     foreach ($same_books as $book) : ?>
-
                         <li>
                             <a href="<?php echo $book->guid; ?>"><?php esc_html_e($book->post_title) ?></a>
                         </li>
-
                     <?php endforeach; ?>
                 </ul>
-                <div class="d-flex justify-content-center button-container">
-                    <a href='<?= home_url() ?>' title='To homepage' class="button bg-dark">Back to homepage</a>
-                </div>
+                    <div>
+                        <?php
+                        $user = wp_get_current_user();
+                        if (get_the_author() == $user->nickname || current_user_can('edit_published_posts')) : ?>
+                            <h5> <?php edit_post_link(); ?> </h5>
+                        <?php endif ?>
+                    </div>
+                    <div class="d-flex justify-content-center button-container">
+                        <a href='<?= home_url() ?>' title='To homepage' class="button bg-dark">Back to homepage</a>
+                    </div>
             </div>
         </div>
     <?php
